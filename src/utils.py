7310cdd38449d0eb378e88777c10f36fe5c930e0
DhruvJais01 <dhruvjaiswal400@gmail.com>
2025-04-04 10:57:53 +0530

    Hyper parameter tunning

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 import dill
 from sklearn.metrics import r2_score
@@ -37,7 +38,7 @@ def save_object(file_path, obj):
         raise CustomException(e, sys)
 
 
-def evaluate_model(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models, params) -> dict:
     """
     Evaluates multiple regression models and returns a report of their performance.
 
@@ -47,6 +48,7 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
         X_test (array-like): Testing feature data.
         y_test (array-like): Testing target data.
         models (dict): Dictionary of model names and their corresponding model instances.
+        params (dict): Dictionary of model names and their corresponding parameter grids.
 
     Returns:
         dict: A dictionary containing model names as keys and their respective R2 scores as values.
@@ -66,13 +68,31 @@ def evaluate_model(X_train, y_train, X_test, y_test, models):
                 "Linear Regression": LinearRegression(),
                 "AdaBoost": AdaBoostRegressor(),
             }
-        >>> evaluate_model(X_train, y_train, X_test, y_test, models)
+        >>> params = {
+                "Random Forest": {"n_estimators": [100, 200], "max_depth": [None, 10, 20]},
+                "Decision Tree": {"max_depth": [None, 10, 20]},
+                "Gradient Boosting": {"n_estimators": [100, 200], "learning_rate": [0.01, 0.1]},
+                "XGBoost": {"n_estimators": [100, 200], "learning_rate": [0.01, 0.1]},
+                "CatBoost": {"iterations": [100, 200], "depth": [6, 8]},
+                "KNeighbors": {"n_neighbors": [3, 5, 7]},
+                "Linear Regression": {},
+                "AdaBoost": {"n_estimators": [50, 100]},
+            }
+        >>> X_train, y_train, X_test, y_test = ...  # Load your data here
+        >>> evaluate_model(X_train, y_train, X_test, y_test, models, params)
         {'Random Forest': 0.841, 'Decision Tree': 0.841, 'Gradient Boosting': 0.841, 'XGBoost': 0.841, 'CatBoost': 0.841, 'KNeighbors': 0.841, 'Linear Regression': 0.841, 'AdaBoost': 0.841}
     """
     try:
         report = {}
         for i in range(len(models)):
             model = list(models.values())[i]
+            param = params[list(models.keys())[i]]
+
+            # Setting the parameters for the model
+            gs = GridSearchCV(model, param_grid=param, cv=3)
+            gs.fit(X_train, y_train)
+            model.set_params(**gs.best_params_)
+
             model.fit(X_train, y_train)
 
             # Predicting on training data
